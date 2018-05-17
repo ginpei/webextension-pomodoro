d1ca94aa04a882dd1d2fbce4944fa6fb75c7f4cf
ginpei <ginpei@gmail.com>
2018-05-17 14:29:30 -0700

    extract popup codes into a class

---
+++ extension/popup/PopupController.js
@@ -0,0 +1,67 @@
+// eslint-disable-next-line no-unused-vars
+class PopupController {
+  get remainingSeconds () {
+    return Math.ceil(this.remaining / 1000);
+  }
+
+  get statusText () {
+    if (this.active) {
+      return this.running ? 'Running' : 'Breaking';
+    }
+    return 'Off';
+  }
+
+  constructor () {
+    this.active = false;
+    this.running = false;
+    this.remaining = 0;
+
+    this.elButtonArea = document.querySelector('.button');
+    this.elStatus = document.querySelector('#status');
+    this.elRemainingArea = document.querySelector('.remaining');
+    this.elRemaining = document.querySelector('#remaining');
+  }
+
+  init () {
+    document.querySelector('#start').onclick = () => {
+      this.start();
+    };
+
+    document.querySelector('#stop').onclick = () => {
+      this.stop();
+    };
+
+    browser.runtime.onMessage.addListener((message) => {
+      if (message.type === 'TIMER_TICK') {
+        this.onTick(message);
+      }
+    });
+  }
+
+  render () {
+    this.elButtonArea.setAttribute('data-active', this.active.toString());
+    this.elRemainingArea.setAttribute('data-active', this.active.toString());
+
+    this.elStatus.textContent = this.statusText;
+    this.elRemaining.textContent = `${this.remainingSeconds} s`;
+  }
+
+  start () {
+    browser.runtime.sendMessage({
+      type: 'TIMER_START',
+    });
+  }
+
+  stop () {
+    browser.runtime.sendMessage({
+      type: 'TIMER_STOP',
+    });
+  }
+
+  onTick (message) {
+    this.active = message.active;
+    this.running = message.running;
+    this.remaining = message.remaining;
+    this.render();
+  }
+}
--- extension/popup/main.js
+++ extension/popup/main.js
@@ -1,33 +1,6 @@
-document.querySelector('#start').onclick = () => {
-  browser.runtime.sendMessage({
-    type: 'TIMER_START',
-  });
-};
+/* global PopupController */
 
-document.querySelector('#stop').onclick = () => {
-  browser.runtime.sendMessage({
-    type: 'TIMER_STOP',
-  });
-};
-
-browser.runtime.onMessage.addListener((message) => {
-  if (message.type === 'TIMER_TICK') {
-    const elButtonArea = document.querySelector('.button');
-    const elStatus = document.querySelector('#status');
-    const elRemainingArea = document.querySelector('.remaining');
-
-    elButtonArea.setAttribute('data-active', message.active.toString());
-    elRemainingArea.setAttribute('data-active', message.active.toString());
-
-    // eslint-disable-next-line no-nested-ternary
-    elStatus.textContent = message.active ? (message.running ? 'Running' : 'Breaking') : 'Off';
-
-    let text = '';
-    if (message.active) {
-      const remainingSeconds = Math.ceil(message.remaining / 1000);
-      text = `${remainingSeconds} s`;
-    }
-    const elRemaining = document.querySelector('#remaining');
-    elRemaining.textContent = text;
-  }
-});
+(() => {
+  const controller = new PopupController();
+  controller.init();
+})();
